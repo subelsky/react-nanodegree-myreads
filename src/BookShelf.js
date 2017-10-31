ed82f4fa9dcb4c8d3c5be198b8544d1cd2ea3e7a
Mike Subelsky <mike@subelsky.com>
2017-10-31 18:21:33 -0400

    refactor: convert BookShelf into stateless functional component
    (H/T Udacity reviewer)

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,34 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Book from './Book'
 import PropTypes from 'prop-types'
 
-class BookShelf extends Component {
-  static propTypes = {
-    onChange: PropTypes.func.isRequired,
-    title: PropTypes.string.isRequired,
-    books: PropTypes.array.isRequired
-  }
+const BookShelf = ({ onChange, title, books }) => {
+  // quick hack to convert from camelcase to title case
+  const titleParts = title.split(/(?=[A-Z])/)
+  const humanTitle = titleParts.map((t) => t.replace(/^(\w)/,(s) => s.toUpperCase())).join(' ')
 
-  render() {
-    const { books } = this.props
-
-    // quick hack to convert from camelcase to title case
-    let titleParts = this.props.title.split(/(?=[A-Z])/)
-    const title = titleParts.map((t) => t.replace(/^(\w)/,(s) => s.toUpperCase())).join(' ')
-
-    return (
-    <div className="bookshelf">
-      <h2 className="bookshelf-title">{title}</h2>
-      <div className="bookshelf-books">
-        <ol className="books-grid">
-        {books.map((book) => (
-          <Book key={book.title} book={book} onChange={this.props.onChange}/>
-        ))}
-        </ol>
-      </div>
+  return (
+  <div className="bookshelf">
+    <h2 className="bookshelf-title">{humanTitle}</h2>
+    <div className="bookshelf-books">
+      <ol className="books-grid">
+      {books.map((book) => (
+        <Book key={book.title} book={book} onChange={onChange}/>
+      ))}
+      </ol>
     </div>
-    )
-  }
+  </div>
+  )
+}
+
+BookShelf.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  books: PropTypes.array.isRequired
 }
 
 export default BookShelf
